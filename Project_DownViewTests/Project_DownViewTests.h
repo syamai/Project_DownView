13fa5cdf78037ebe04dc76069ba539bd0b3ce0d5
syamai <selab.ahn@gmail.com>
2012-11-19 00:06:47 +0900

    aaa

---
+++ Project_DownViewTests/Project_DownViewTests.h
@@ -0,0 +1,13 @@
+//
+//  Project_DownViewTests.h
+//  Project_DownViewTests
+//
+//  Created by syamai on 12. 11. 15..
+//  Copyright (c) 2012년 syamai. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Project_DownViewTests : SenTestCase
+
+@end
---
+++ Project_DownViewTests/Project_DownViewTests.m
@@ -0,0 +1,32 @@
+//
+//  Project_DownViewTests.m
+//  Project_DownViewTests
+//
+//  Created by syamai on 12. 11. 15..
+//  Copyright (c) 2012년 syamai. All rights reserved.
+//
+
+#import "Project_DownViewTests.h"
+
+@implementation Project_DownViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Project_DownViewTests");
+}
+
+@end
